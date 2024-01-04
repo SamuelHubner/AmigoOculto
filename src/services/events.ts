cc60e5763ec339a78f5d66b9ba1a69d3ee39a16d
Samuel Hubner <samuelthubner@gmail.com>
2024-01-03 21:05:29 -0300

    feat: doMatches function

--- src/services/events.ts
+++ src/services/events.ts
@@ -1,4 +1,7 @@
 import { PrismaClient, Prisma } from '@prisma/client';
+import * as peopleService from './people';
+import * as groupsService from './groups';
+
 const prisma = new PrismaClient();
 
 export const getAll = async () => {
@@ -50,7 +53,44 @@ export const remove = async (id: number) => {
 export const doMatches = async (id: number): Promise<boolean> => {
     const eventItem = await prisma.event.findFirst({where: {id}, select: {grouped: true}});
     if (eventItem) {
+        const peopleList = await peopleService.getAll(id);
+        if (peopleList) {
+            let sortedList:{id:number, match:number}[] = [];
+            let sortable: number [] = [];
 
+            let attempts = 0;
+            let maxAttempts = peopleList.length;
+            let keepTrying = true;
+            while (keepTrying && attempts < maxAttempts) {
+                keepTrying = false;
+                attempts++;
+                sortedList = [];
+                sortable = peopleList.map(item => item.id);
+                
+                for (let i in peopleList) {
+                    let sortableFiltered: number[] = sortable;
+                    if (eventItem.grouped) {
+                        sortableFiltered = sortable.filter(
+                            sortableItem => {
+                                let sortablePerson = peopleList.find(item => item.id === sortableItem);
+                                return peopleList[i].id_group !== sortablePerson?.id_group;
+                            }
+                         );
+                    }
+                }
+            }
+
+            if (attempts < maxAttempts) {
+                for (let i in sortedList) {
+                    await peopleService.update({
+                        id: sortedList[i].id,
+                        id_event: id,
+                    }, {matched: ''})
+                }
+
+                return true;
+            }
+        }
     }
     return true;
 }
