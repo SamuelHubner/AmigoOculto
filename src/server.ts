99cee50026f3f5941290c68e024758346aafa481
Samuel Hubner <samuelthubner@gmail.com>
2024-01-08 15:54:56 -0300

    chore: add production server config.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import siteRoutes from './routes/site';
 import { requestInterceptor } from './utils/requestIntercepter';
 import adminRoutes from './routes/admin';
@@ -25,7 +27,14 @@ const runServer = (port: number, server: http.Server) => {
 
 const regularServer = http.createServer(app);
 if ( process.env.NODE_ENV === 'production' ) { 
-    console.log('Production server');
+    const option = {
+        key: fs.readFileSync(process.env.SSL_KEY as string || ''),
+        cert: fs.readFileSync(process.env.SSL_CERT as string || '')
+    };
+
+    const secServer = https.createServer(option, app);
+    runServer(80, regularServer);
+    runServer(443, secServer);
 } else {
     const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
     runServer(serverPort, regularServer);
